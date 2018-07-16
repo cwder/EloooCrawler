ae06600fa09415be6c1197f35abe57badafe88a3
elooocao <cwder@126.com>
2018-07-17 00:55:43 +0800

    进一步解析

---
+++ Action/brazil.py
@@ -0,0 +1,17 @@
+from Action.Base import Base
+
+
+class Brazol(Base):
+    url = "http://zq.win007.com/cn/League/4.html"
+
+    def __init__(self):
+        self.team_dict = self.get_team_array(self.url)
+
+
+if __name__ == "__main__":
+
+    kk = Brazol()
+
+    for k, v in kk.team_dict.items():
+        print(k)
+        print(kk.parse_team(v))
