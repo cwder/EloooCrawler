4344c0c32f0c0d686809ebaa8f584b758ea7fbd5
elooocao <cwder@126.com>
2018-07-18 00:47:40 +0800

    进一步解析

---
+++ Action/japan_one.py
@@ -0,0 +1,19 @@
+from Action.Base import Base
+
+
+class JapanOne(Base):
+    url = "http://zq.win007.com/cn/SubLeague/25.html"
+
+    def __init__(self):
+        self.team_dict = self.get_team_array(self.url)
+
+
+if __name__ == "__main__":
+
+    kk = JapanOne()
+
+    for k, v in kk.team_dict.items():
+        print(k)
+        print(kk.parse_team(v))
+        # print(kk.parse_fail_team(v[:50]))
+
