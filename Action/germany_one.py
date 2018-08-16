a80d460432352a283d92d10a18f11967424f34b8
wenda.cao <wenda.cao@chinapnr.com>
2018-08-16 14:42:35 +0800

    联赛解析

---
+++ Action/germany_one.py
@@ -0,0 +1,17 @@
+from Action.Base import Base
+
+
+class GermanyOne(Base):
+    url = "http://zq.win007.com/cn/League/8.html"
+
+    def __init__(self):
+        self.team_dict = self.get_team_array(self.url)
+
+
+if __name__ == "__main__":
+
+    kk = GermanyOne()
+
+    for k, v in kk.team_dict.items():
+        print(k)
+        print(kk.parse_team(v))
