5cf54799621c9c5ebfdfc439bbb414851d6879c5
elooocao <cwder@126.com>
2018-08-16 23:50:21 +0800

    进一步解析

---
+++ Action/tu_chao.py
@@ -0,0 +1,18 @@
+from Action.Base import Base
+
+
+class Tuchao(Base):
+    url = "http://zq.win007.com/cn/SubLeague/30.html"
+
+    def __init__(self):
+        self.team_dict = self.get_team_array(self.url)
+
+
+if __name__ == "__main__":
+
+    kk = Tuchao()
+
+    for k, v in kk.team_dict.items():
+        print(k)
+        # print(kk.parse_team(v))
+        print(kk.parse_fail_team(v))
